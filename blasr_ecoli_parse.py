e82b5e2af0a260f76a78091fb9241778be13c098
Erin Turley <erinturley@live.com>
2017-12-10 20:06:57 -0500

    calculating percentages now

--- blasr_ecoli_parse.py
+++ blasr_ecoli_parse.py
@@ -3,6 +3,7 @@ import collections
 
 with open("blasr_ecoli.sam", 'r') as sam_file:
 	#add each line to a dictionary of lists
+	numlines = 0
         d = collections.defaultdict(list)
 	for line in sam_file:
        	    if line.startswith('m'):
@@ -15,6 +16,7 @@ with open("blasr_ecoli.sam", 'r') as sam_file:
 				    d[id_num].append(line)
 		    else:
 			    d[id_num].append(line)
+	    numlines += 1
 	new_d = collections.defaultdict(list)
 	#iterate over dict, saving lines with a 0 and 16
 	for k, v in d.iteritems():
@@ -22,6 +24,7 @@ with open("blasr_ecoli.sam", 'r') as sam_file:
 		keep = 0
 		for i in v:
 			alignment = i.split()[1]
+			sz = i.split()[9]
 			if(ct == 0):
 				prev = alignment
 				ct += 1
@@ -29,6 +32,7 @@ with open("blasr_ecoli.sam", 'r') as sam_file:
 				curr = alignment
 				if(curr == prev):
 					keep = 0
+				#elif(len(sz) > 10000):
 				else:
 					keep = 1
 				ct = 0
@@ -37,9 +41,37 @@ with open("blasr_ecoli.sam", 'r') as sam_file:
 				new_d[k].append(j)
 
 	#return the sizes
+	VAL = 0
+#	insertions = list()
+#	deletions = list()
+#	substitutions = list()
 	for k, v in new_d.iteritems():
+#		if(VAL > 10):
+#			break
+#		ins = 0
+#		dels = 0
+#		subs = 0
 		print k, ": "
 		for j in v:
 			strand = j.split()[9]
+#			info = j.split()[5]
+#			info = info.split('=')
+#			for m in info:
+#				r = re.compile("([0-9]+)([a-zA-Z]+)([0-9]+)")
+#				res = r.match(m)
+#				if res:
+#					num_occurrences = int(res.group(1))
+#					chartype = res.group(2)
+#				if(chartype == 'I'):
+#					ins += num_occurrences
+#				elif(chartype == 'S'):
+#					subs += num_occurrences
+#				elif(chartype == 'D'):
+#					dels += num_occurrences
 			print "\t", len(strand)
+#		print "\tinsertions: ", ins, ", subs: ", subs, ", deletions: ", dels
+#		VAL += 1
+		
 	print "Total inconsistencies: ",len(new_d)
+	print "Total number of lines: ", numlines
+	print "Percentage: ", 100 * float(len(new_d))/float(numlines)
